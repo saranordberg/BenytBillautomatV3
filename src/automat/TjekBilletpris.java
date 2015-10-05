79d1768a28189dd4a077a46c59ba7f46f60847b1
Sara Nordberg <saranordberg@me.com>
2015-10-05 15:48:43 +0200

    første commit

---
+++ src/automat/TjekBilletpris.java
@@ -0,0 +1,28 @@
+package automat;
+
+public class TjekBilletpris
+{
+	public static void main(String[] arg)
+	{
+		Billetautomat automat = new Billetautomat();
+
+		int antalFejl = 0;
+		int retur;
+		
+		automat.setBilletpris(20); //må ikke kunne ændre billetprisen uden kode
+		retur = automat.getBilletpris();
+		if (retur != 10) {
+			System.out.println("FEJL, getBilletpris() != 10");
+		}
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Der blev fundet "+antalFejl+" fejl i automaten");
+		if (antalFejl == 0) {
+			System.out.println("Automaten er fejlfri, HURRA!!!");
+		} else {
+			System.out.println("Automaten har fejl, ØV!!!");			
+		}
+	}
+}
---
+++ src/automat/TjekNegativeBeloeb.java
@@ -0,0 +1,36 @@
+package automat;
+
+public class TjekNegativeBeloeb
+{
+	public static void main(String[] arg)
+	{
+		Billetautomat automat = new Billetautomat();
+
+		int antalFejl = 0;
+		int retur;
+		
+		retur = automat.getBilletpris();
+		if (retur != 10) {
+			System.out.println("FEJL, getBilletpris() != 10");
+		}
+
+		System.out.println("*** Tjek af salg af en billet");
+		automat.indsætPenge(-10); //Negativ beløb indsat
+		automat.udskrivBillet();
+		retur = automat.returpenge();
+		if (retur != 0) {
+			System.out.println("FEJL, returpenge() skulle give 0, men den gav: "+retur);
+			antalFejl = antalFejl + 1;
+		}
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Der blev fundet "+antalFejl+" fejl i automaten");
+		if (antalFejl == 0) {
+			System.out.println("Automaten er fejlfri, HURRA!!!");
+		} else {
+			System.out.println("Automaten har fejl, ØV!!!");			
+		}
+	}
+}
